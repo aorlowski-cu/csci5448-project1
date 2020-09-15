22322181921d0ac96cf0ae45f18f80c856df8c8d
Lincoln <lincoln.schafer@icloud.com>
2020-09-15 09:50:38 -0600

    Edited path name of output text file as per rubric, gave zooEmployee and zooKeeper objects names, printed said name, allowed the program to ask how many days that you want it to run for, as requested in the rubric

--- src/com/csci5448/zoo/ZooApp.java
+++ src/com/csci5448/zoo/ZooApp.java
@@ -7,21 +7,24 @@ import com.csci5448.zoo.employees.Zookeeper;
 import java.io.File;
 import java.io.IOException;
 import java.io.PrintStream;
+import java.util.Scanner;
 
 public class ZooApp {
 
     public static void main(String[] args) throws IOException {
 
+        Scanner s = new Scanner(System.in);
         // setting numOfDay as a period of 7
-        int numOfDay = 7;
-
+        System.out.println("How many days do you want to run the program for?");
+        int numOfDay = s.nextInt();
+        s.close();
         Zoo.initZoo();
 
         Zookeeper keeper = new Zookeeper();
 
         // https://www.tutorialspoint.com/redirecting-system-out-println-output-to-a-file-in-java#:~:text=Instantiate%20a%20PrintStream%20class%20by,created%20in%20the%20first%20step.
         // Direct output stream to file 'out.txt'
-        File file = new File("out.txt");
+        File file = new File("dayatthezoo.out");
         PrintStream stream = new PrintStream(file);
         System.setOut(stream);
 
--- src/com/csci5448/zoo/employees/ZooEmployee.java
+++ src/com/csci5448/zoo/employees/ZooEmployee.java
@@ -1,11 +1,27 @@
 package com.csci5448.zoo.employees;
 
+import com.csci5448.zoo.utils.Namer;
+
 public abstract class ZooEmployee {
+
+    public ZooEmployee() {
+        String firstLetter = this.getClass().getSimpleName().substring(0,1);
+        this._name = Namer.getNewName(firstLetter);
+    }
+
+    public String getName() {
+        return _name;
+    }
+
+    private String _name;
+
     public void arrive(int day){
-        System.out.println(String.format("The %s arrived at the zoo on Day %d.", this.getClass().getSimpleName(), day));
+        System.out.println(String.format("The %s, %s arrived at the zoo on Day %d.", this.getClass().getSimpleName(),this.getName(), day));
     }
 
     public void depart(){
-        System.out.println(String.format("The %s departed the zoo.", this.getClass().getSimpleName()));
+        System.out.println(String.format("The %s, %s departed the zoo.", this.getClass().getSimpleName(),this.getName()));
     }
+
+
 }
