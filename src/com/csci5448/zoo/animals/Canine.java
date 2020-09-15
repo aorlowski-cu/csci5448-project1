9627831cd5f6efd36195cc7a5d1a457ca0c759a0
Lincoln <lincoln.schafer@icloud.com>
2020-09-15 10:22:45 -0600

    Fixed dog method being in Canine class

--- src/com/csci5448/zoo/animals/Canine.java
+++ src/com/csci5448/zoo/animals/Canine.java
@@ -7,19 +7,6 @@ public class Canine extends Animal{
         super();
     }
 
-    // Override superclass Animal's exercise method
-    @Override
-    public void exercise() {
-
-        Random rand = new Random();
-        int decision = rand.nextInt(4);
-        if(decision == 0){
-            System.out.println(String.format("%s the %s dug.",this.getName(), this.getClass().getSimpleName()));
-        } else {
-            System.out.println(String.format("%s the %s roamed.", this.getName(), this.getClass().getSimpleName()));
-        }
-    }
-
     // Override superclass Animal's sleep method
     @Override
     public boolean sleep() {
--- src/com/csci5448/zoo/animals/Dog.java
+++ src/com/csci5448/zoo/animals/Dog.java
@@ -1,10 +1,24 @@
 package com.csci5448.zoo.animals;
 
+import java.util.Random;
+
 public class Dog extends Canine{
     public Dog(){
         super();
     }
 
+    // Override superclass Canine's exercise method
+    @Override
+    public void exercise() {
+
+        Random rand = new Random();
+        int decision = rand.nextInt(4);
+        if(decision == 0){
+            System.out.println(String.format("%s the %s dug.",this.getName(), this.getClass().getSimpleName()));
+        } else {
+            System.out.println(String.format("%s the %s roamed.", this.getName(), this.getClass().getSimpleName()));
+        }
+    }
     // Override superclass Canine's makeNoise method
     @Override
     public void makeNoise() {
