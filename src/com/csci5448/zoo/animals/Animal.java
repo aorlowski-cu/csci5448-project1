658e09e1c783f6fa4d59f97163593ea462a2e9f8
Lincoln <lincoln.schafer@icloud.com>
2020-09-16 10:35:35 -0600

    Added Object Oriented example documentation

--- src/com/csci5448/zoo/animals/Animal.java
+++ src/com/csci5448/zoo/animals/Animal.java
@@ -2,17 +2,18 @@ package com.csci5448.zoo.animals;
 
 import com.csci5448.zoo.utils.Namer;
 
-public abstract class Animal {
+public abstract class Animal { //Abstract class example,
     public Animal() {
         String firstLetter = this.getClass().getSimpleName().substring(0,1);
+        //Identity example, giving each object a unique name
         this._name = Namer.getNewName(firstLetter);
     }
 
     public String getName() {
         return _name;
-    }
+    } //Encapsulation, getter method for private variable
 
-    private String _name;
+    private String _name; //Encapsulation example, private instance variable for animals
     public boolean sleep(){
         // string format from https://dzone.com/articles/java-string-format-examples
         // get class name from https://stackoverflow.com/questions/6901764/get-concrete-class-name-from-abstract-class
--- src/com/csci5448/zoo/employees/ZooEmployee.java
+++ src/com/csci5448/zoo/employees/ZooEmployee.java
@@ -2,18 +2,18 @@ package com.csci5448.zoo.employees;
 
 import com.csci5448.zoo.utils.Namer;
 
-public abstract class ZooEmployee {
+public abstract class ZooEmployee { //Abstract class example, also Abstraction
 
     public ZooEmployee() {
-        String firstLetter = this.getClass().getSimpleName().substring(0,1);
+        String firstLetter = this.getClass().getSimpleName().substring(0,1); //Identity example, giving each object a unique name
         this._name = Namer.getNewName(firstLetter);
     }
 
     public String getName() {
         return _name;
-    }
+    }//Encapsulation example
 
-    private String _name;
+    private String _name; //encapsulation example
 
     public void arrive(int day){
         System.out.println(String.format("The %s, %s arrived at the zoo on Day %d.", this.getClass().getSimpleName(),this.getName(), day));
